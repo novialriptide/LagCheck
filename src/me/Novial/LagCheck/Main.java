2dc48e3962b516767b6e07a749b528d2133a4856
NovialRiptide <35881688+NovialRiptide@users.noreply.github.com>
2022-01-22 18:41:11 -0500

    Removed unused code

--- src/me/Novial/LagCheck/Main.java
+++ src/me/Novial/LagCheck/Main.java
@@ -1,18 +1,13 @@
 package me.Novial.LagCheck;
 
-import me.Novial.LagCheck.Events;
-
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import net.md_5.bungee.api.ChatColor;
 
 public class Main extends JavaPlugin {
 	@Override
 	public void onEnable() {
 		Bukkit.getServer().getScheduler().scheduleSyncRepeatingTask(this, new tps(), 100L, 1L);
 		getCommand("stats").setExecutor(new Commands());
-		getServer().getPluginManager().registerEvents(new Events(), this);
 		getServer().getConsoleSender().sendMessage("Enabling LagCheck");
 	}
 	
